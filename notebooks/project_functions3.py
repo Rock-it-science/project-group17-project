43d23dd92296fceaec242238be443b0603bdf47b
sliwong <sarahliwong@hotmail.com>
2021-11-02 17:54:16 -0700

    Task Updates

---
+++ notebooks/project_functions3.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+def unprocessed(csv_file):
+    tracks = pd.read_csv(csv_file)
+    return tracks
+
+# Method chain to load and process tracks data for analysis 3
+def chain(\tracks_csv="../data/raw/tracks.csv"):
+    
+
+def sort_genre(dfp):
+    
