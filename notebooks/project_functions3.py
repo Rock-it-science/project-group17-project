de27298461b5f19451c38d74240e1f0ad920f725
sliwong <sarahliwong@hotmail.com>
2021-11-26 06:13:02 -0800

    finished milestone

--- notebooks/project_functions3.py
+++ notebooks/project_functions3.py
@@ -1,13 +1,25 @@
 import pandas as pd
 import numpy as np
 
-def unprocessed(csv_file):
-    tracks = pd.read_csv(csv_file)
-    return tracks
-
-# Method chain to load and process tracks data for analysis 3
-#def chain(\tracks_csv="../data/raw/tracks.csv"):
+#Method chaining function (Load and process data; deal with missing data, etc.)
+def unprocessed(tracks_csv="../data/raw/tracks.csv"):
+    #Loading data
+    data = pd.read_csv(tracks_csv, low_memory=False) 
+    data = (
+        data
+        
+        #Remove row 1
+        .drop(data.index[1])
+        
+        #Select needed columns that we will be using for the analysis
+        .loc[:, ['Unnamed: 0','track.7', 'track.14', 'artist.12', 'artist.9', 'album.2']]
+        
+        #Rename columns that we will be using for the analysis
+        .rename(columns={'Unnamed: 0': 'Track ID', 'track.7': 'Genre', 'track.14': 'Listens', 'artist.12': 'Artist Name', 'artist.9': 'Location', 'album.2': 'Release Date'})
+       
+        #Drop rows with na values
+        .dropna()
+    )
     
-
-#def sort_genre(dfp):
+        return data
     
